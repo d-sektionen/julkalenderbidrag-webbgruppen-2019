32d0ff057358a38964d2433318771f5e30452cd2
William Lindén <willi125@student.liu.se>
2019-11-15 20:55:41 +0100

    hit detection, movement  and score added

--- source.js
+++ source.js
@@ -1,10 +1,21 @@
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
+var score = 0;
+var totalScore = 0;
+
+var spawnRate = 1000;
+
+var xCoord = 0;
+var circleR = 15;
+
+function mouseX(event){
+    xCoord = event.clientX;
+}
 
 function circle(x,y){
     ctx.beginPath();
-    ctx.arc(x, y, 15, 0, 2 * Math.PI);
+    ctx.arc(x, y, circleR, 0, 2 * Math.PI);
     ctx.fillStyle= "red";
     ctx.fill(); 
 }
@@ -20,11 +31,28 @@ function randStartCoord(){
 
 function update(){
     ctx.clearRect(0, 0, c.width, c.height);
+    player.x = xCoord - 50;
     player.draw();
-    caps.forEach(function(element){
+    for (let i = 0; i < caps.length; i++) {
+        const element = caps[i];
         element.draw();
         element.move();
-    });
+
+        if (element.y > c.height){
+            caps.splice(i, 1);
+            score--;
+            console.log(score);
+        }
+        if (player.y - element.y <= circleR && element.x - player.x < 100 && element.x - player.x > 0) {
+            caps.splice(i, 1);
+            score++;
+            totalScore++;
+            if (totalScore >= 10){
+                spawnRate *= 0,5; //TODO: FUNKAR INTE ?!?!?!??!?!?!?!
+            }
+            console.log(score)
+        }
+    }
 }
 
 function spawnCap(){
@@ -40,14 +68,13 @@ class BottleCap{
         circle(this.x,this.y);
     }
     move(){
-        this.y += 3;
+        this.y += 5;
     }
 }
 
 class Cup{
     constructor(){
-        //TODO: do if mouse pressed down
-        this.x = 30; //TODO: Mouse x-coord
+        this.x = 30; 
         this.y = c.height-20;
     }
     draw(){
@@ -58,4 +85,4 @@ var caps = [];
 var player = new Cup();
 
 setInterval(update, 17);
-setInterval(spawnCap, 1000);
+setInterval(spawnCap, spawnRate);
